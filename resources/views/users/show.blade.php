244a60271ce6ea2b16c840b49d9c0b71a28da0d4
Marija Juric <mjuric12@vvg.hr>
2021-01-20 21:30:37 +0100

    poruka

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -10,6 +10,21 @@
         <li>Phone number: {{$user->phone_number }}</li>
         
     </ul>
+    <div>
+    @if($loggedInUser->isAdmin() || $user->id === $loggedInUser->id)
+        <a class="btn btn-primary" href="{{ route('users.edit', ['user' => $user]) }}">Edit</a>
+        <a class="btn btn-primary" href="{{ route('change_password.edit', ['user' => $user]) }}">Change Password</a>
+        @endif
+
+    @if($loggedInUser->isAdmin() || $user->id === $loggedInUser->id)
+        <form class="form-inline" action="{{ route('users.destroy', ['user' => $user->id]) }}" method="POST">
+            <!-- CSRF token -->
+            @csrf
+            @method('DELETE')
+            <button type="submit" onclick="confirm('Are you sure?')" class="btn btn-danger">Deactivate</button>
+        </form>
+        @endif
+    </div>
     <a href="{{ route('users.index')}}" class="btn btn-outline-secondary"> Back </a>
 </div>
 
